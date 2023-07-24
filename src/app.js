70cd17139eb59003dbd31946521ab008d290356f
Shahid Riaz Bhatti <shahid.bhatti@hotmail.com>
2023-07-24 23:16:14 +0500

    Completed the Publisher and added express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+import express, { json } from "express"
 import { PutMessageToQueue } from "./messagebroker/message-broker-rabbitmq.js";
 import dotenv  from "dotenv"
 
+const PORT = "5059";
 dotenv.config();
 
 
-let message = '"{"message":"Hello"}"';
-console.log(message);
-PutMessageToQueue(message);
+const app = express();
+app.use(express.json());
+//Write a Post method which accepts call the PutMessageToQueue from the request body
+app.post("/", (req,res)=>{
+    const message = req.body;
+    PutMessageToQueue(JSON.stringify(message));    
+    console.info("This code will be executed just after sending the call to send message to queue and will continiue doing its job");
+    for(let i = 0; i < 100; i ++)
+    {
+        console.log(`${i} x 2 = ${i*2}`);
+    }
+    res.send(req.body);
+});
+app.listen(PORT, ()=>{
+    console.info(`Server is listening on PORT ${PORT}`);
+});
--- src/messagebroker/message-broker-rabbitmq.js
+++ src/messagebroker/message-broker-rabbitmq.js
@@ -3,22 +3,27 @@ import amqp from "amqplib" // imported this package to induct connection and int
 // Write a method which is responsible to connect with the rabbitmq-server
 async function connectWithRabbitMqAndGiveChannel(){
     try {
-        const username = process.env.username;
-        console.log(`User Name is ${username}`);
-        const password = process.env.password;
-        console.log(`password is ${password}`)
-        const host = process.env.RABBITMQ_HOST;
-        console.log(`RabbitMQ Server URL  is ${host}`)
-        const port = process.env.RABBITMQ_PORT;
-        console.log(`RabbitMQ Server Port is ${port}`)
-        const url = `amqp://${username}:${password}@${host}:${port}`;
-        console.log(`Complete connection String is ${url}`)
-        const connection = await amqp.connect(url);
-        const channel = await connection.createChannel();
-        console.error('Connected Successfully!:');
-        return channel;
-      } catch (error) {
-        console.error('Error connecting to RabbitMQ:', error);
+            const username = process.env.username;
+            console.log(`User Name is ${username}`);
+            const password = process.env.password;
+            console.log(`password is ${password}`)
+            const host = process.env.RABBITMQ_HOST;
+            console.log(`RabbitMQ Server URL  is ${host}`)
+            const port = process.env.RABBITMQ_PORT;
+            console.log(`RabbitMQ Server Port is ${port}`)
+            const url = `amqp://${username}:${password}@${host}:${port}`;
+            console.log(`Complete connection String is ${url}`)
+            const connection = await amqp.connect(url);
+            connection.on('error', (err)=>{
+                console.error(`Error ${err} while establishing the connection with rabbitmq`)
+            });
+            const channel = await connection.createChannel();
+            channel.on('error', (err)=>{
+                console.error(`There error ${err} occured while creating the channel`);
+            });
+            return channel;
+         } catch (error) {
+        console.error('Error occured:', error);
       }
 }
 //Method to Send message to the queue
@@ -30,16 +35,16 @@ async function PutMessageToQueue(message)
     {
         ch = await connectWithRabbitMqAndGiveChannel();
     }catch(error){
-        console.log(`An error is occured while connecting with the queue named ${queue_name}`);
+        console.error(`An error is occured while connecting with the queue named ${queue_name}`);
         return;
     }
     try{
-        ch.sendToQueue(queue_name, Buffer.from(message));
+        ch.sendToQueue(queue_name, Buffer.from(message), { persistent: true });
+
     }catch(error){
-        console.log(`unable to send message to the queue ${queue_name}`)
+        console.error(`unable to send message to the queue ${queue_name} due to error ${error}`)
         return;
     }
     console.log(`Message sent to the queue ${queue_name}`);
 }
-// Exported PutMessageToQueue
 export {PutMessageToQueue}
